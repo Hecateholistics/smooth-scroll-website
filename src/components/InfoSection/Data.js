035ef5f5bcc07abe3285715b9d49be65d764d96e
diego_arteaga <diego.arteagaquevedo@gmail.com>
2021-12-01 12:23:36 +0100

    primercommit

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+
+
+export const homeObj = {
+    id: 'about',
+    lightBg: true,
+    lightText: false,
+    topLine: 'Premiun Services',
+    headline: 'Unlimited ',
+    description: 'Get extra value',
+    buttonLabel: 'Get started!',
+    imgStart: false,
+    img: require('../../images/asset_selection.svg'),
+    alt: 'car',
+    dark: true,
+    primary: true,
+    darkText: true
+}
