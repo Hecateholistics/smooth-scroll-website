972195c1cf4f5b654e47fef7786beb1d65286188
diego_arteaga <diego.arteagaquevedo@gmail.com>
2022-02-01 12:07:17 +0100

    0102-n2

--- src/Application/routes.js
+++ src/Application/routes.js
@@ -6,7 +6,7 @@ export default () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="signin/" element={<SigninPage />} />
+            <Route path="/signin" element={<SigninPage />} />
             { /* Es muy recomendable añadir esta ruta para obtener un mensaje de error en el caso de que la ruta no exista. De lo contrario, si la ruta no existe llegaremos a una página en blanco */}
             <Route path="*" element={<div>404</div>} />
         </Routes>
